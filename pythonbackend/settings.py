f83535ae9fe7009f58a588242beed29c655bf0ba
rainycitycoder <cvoitmembership@proton.me>
2025-09-06 15:56:12 -0700

    Adjusted code per https://render.com/docs/deploy-django#updating-an-existing-django-project & https://www.youtube.com/watch?v=1nTx0p1QqlM&list=PLmEKHA8iFrmCAPgSe9ze8RXXdE0M11suR&index=4.

--- pythonbackend/settings.py
+++ pythonbackend/settings.py
@@ -96,10 +96,10 @@ WSGI_APPLICATION = 'pythonbackend.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
