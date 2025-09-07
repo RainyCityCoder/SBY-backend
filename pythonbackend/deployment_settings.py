202731ce9608a6bdeb5682a8d2095c397b5c4487
rainycitycoder <cvoitmembership@proton.me>
2025-09-07 15:12:58 -0700

    Adding .gitignore.

--- pythonbackend/deployment_settings.py
+++ pythonbackend/deployment_settings.py
@@ -2,10 +2,13 @@ import os
 import dj_database_url
 from .settings import *
 from .settings import BASE_DIR
+from dotenv import load_dotenv
 
+load_dotenv()
+
+SECRET_KEY = os.environ.get('SECRET_KEY', default='super secret key')
 ALLOWED_HOSTS = [os.environ.get['RENDER_EXTERNAL_HOSTNAME']]
 CSFR_TRUSTED_ORIGINS = ['https://'+os.environ.get['RENDER_EXTERNAL_HOSTNAME']]
-SECRET_KEY = os.environ.get['SECRET_KEY']
 
 DEBUG = False
 
@@ -40,7 +43,15 @@ STORAGES = {
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True,
     )
 }
+DATABASES['default']['OPTIONS'] = {"sslmode": "require"}
+
+STATIC_URL = '/static/'
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    
--- pythonbackend/settings.py
+++ pythonbackend/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.environ.get('SECRET_KEY', default='super secret key')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^t_vm(%06w=p$k+ji7g@n&g1sl8h*l5=0r)9jnfb_9pf8o41&y'
+# SECRET_KEY = 'django-insecure-^t_vm(%06w=p$k+ji7g@n&g1sl8h*l5=0r)9jnfb_9pf8o41&y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -88,19 +93,14 @@ WSGI_APPLICATION = 'pythonbackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+DATABASES['default']['OPTIONS'] = {"sslmode": "require"}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
